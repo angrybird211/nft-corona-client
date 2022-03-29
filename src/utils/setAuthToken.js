9086e7384f9d9d335cfb389c8281db0f5f1901d0
sharp211 <95362611+sharp211@users.noreply.github.com>
2022-03-29 17:41:56 +0800

    feat: changed server url

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -3,10 +3,10 @@ import axios from "../api";
 const setAuthToken = token => {
   if (token) {
     // Apply authorization token to every request if logged in
-    axios.defaults.headers.common["authToken"] = token;
+    axios.defaults.headers.common["x-auth-token"] = token;
   } else {
     // Delete auth header
-    delete axios.defaults.headers.common["authToken"];
+    delete axios.defaults.headers.common["x-auth-token"];
   }
 };
 export default setAuthToken;
